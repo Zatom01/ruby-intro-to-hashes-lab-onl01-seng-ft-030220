a7b86b1d2bfdd1d107fe669b2945ef723496b970
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-07 06:39:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,13 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  railroads:{}
+	  railroads:{
+	    pieces:4,
+	    names:{},
+	    rent_in_dollars:{}
+	  }
 	  
-	}'re going to add stuff to it here!
+	}
 
 end
 
